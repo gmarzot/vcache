2417be9dc90899b8620e486902405ce90dea4117
G S Marzot <gmarzot@vivoh.com>
2023-04-06 12:31:57 -0400

    start supporting version.txt file, dont include possibly irrelevant .env

--- make-release.sh
+++ make-release.sh
@@ -4,14 +4,16 @@ find . -name '*~' | xargs rm -f
 find . -name '#*' | xargs rm -f
 find . -name '.#*' | xargs rm -f
 
-rm -f log/*/*/log
+rm -f log/*/*.log
 rm -f run/*.sock run/uuid.txt  run/*.sock
+
 mkdir release
 
 GIT_REVISION=$(git describe)
+echo ${GIT_REVISION} > run/version.txt
 
 cd ..
 mv vivoh-cache vivoh-cache-${GIT_REVISION}
-./vivoh-cache-${GIT_REVISION}/makeself.sh --nox11 --notemp --tar-extra "--exclude=make* --exclude=.venv --exclude=.npm --exclude=release --exclude=log/* --exclude=run/* --exclude=.git --exclude=.gitignore ./log ./run" --license LICENSE vivoh-cache-${GIT_REVISION} vivoh-cache-${GIT_REVISION}/release/vivoh-cache-${GIT_REVISION}.run "Vivoh Video Cache" ./vivoh-cache-setup.sh
+./vivoh-cache-${GIT_REVISION}/makeself.sh --nox11 --notemp --tar-extra "--exclude=make* --exclude=.venv --exclude=.npm --exclude=release --exclude=log/* --exclude=run/* --exclude=.git --exclude=.env --exclude=.gitignore ./log ./run" --license LICENSE vivoh-cache-${GIT_REVISION} vivoh-cache-${GIT_REVISION}/release/vivoh-cache-${GIT_REVISION}.run "Vivoh Video Cache" ./vivoh-cache-setup.sh
 
 mv vivoh-cache-${GIT_REVISION} vivoh-cache

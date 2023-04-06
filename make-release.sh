875f1edbd97044c73e75de6077ffcf0000ae507f
G S Marzot <gmarzot@vivoh.com>
2023-04-06 15:21:18 -0400

    add sha256 to make-release, activate root path 200 for admin server

--- make-release.sh
+++ make-release.sh
@@ -14,6 +14,6 @@ echo ${GIT_REVISION} > version.txt
 
 cd ..
 mv vivoh-cache vivoh-cache-${GIT_REVISION}
-./vivoh-cache-${GIT_REVISION}/makeself.sh --nox11 --notemp --tar-extra "--exclude=make* --exclude=.venv --exclude=.npm --exclude=release --exclude=log/* --exclude=run/* --exclude=.git --exclude=.env --exclude=.gitignore ./log ./run" --license LICENSE vivoh-cache-${GIT_REVISION} vivoh-cache-${GIT_REVISION}/release/vivoh-cache-${GIT_REVISION}.run "Vivoh Cache" ./vivoh-cache-setup.sh
+./vivoh-cache-${GIT_REVISION}/makeself.sh --sha256 --nox11 --notemp --tar-extra "--exclude=make* --exclude=.venv --exclude=.npm --exclude=release --exclude=log/* --exclude=run/* --exclude=.git --exclude=.env --exclude=.gitignore ./log ./run" --license LICENSE vivoh-cache-${GIT_REVISION} vivoh-cache-${GIT_REVISION}/release/vivoh-cache-${GIT_REVISION}.run "Vivoh Cache" ./vivoh-cache-setup.sh
 
 mv vivoh-cache-${GIT_REVISION} vivoh-cache

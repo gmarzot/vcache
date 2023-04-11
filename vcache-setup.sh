ce8a2f189c483de225a384bb7796eee52b76852c
G S Marzot <gmarzot@vivoh.com>
2023-04-11 15:56:42 -0400

    package setup

--- vcache-setup.sh
+++ vcache-setup.sh
@@ -3,22 +3,31 @@
 type -a docker-compose  > /dev/null
 
 if [ $? != 0 ]; then
-	echo "WARNING: vcache requires docker.io and docker-compose. please install these packages before running vcache-start"
+	echo "unable to find docker-compose, please install docker.io and docker-compose, and provide sudo access..."
+	exit 1
 fi
 
 sudo docker-compose version
 
 if [ $? != 0 ]; then
-	echo "sudo access to docker-compose required, exiting..."
-	exit 1
+	echo "unable to execute sudo docker-compose, please provide sudo access to docker-compse..."
+	exit 2
 fi
 
-logfile=log/vcache-build-$(date '+%Y-%m-%d:%H:%M:%S').log
-sudo docker-compose build | tee ${logfile}
+if [[ -d log && -w log ]]; then
+	logfile=log/vcache-build-$(date '+%Y-%m-%d:%H:%M:%S').log
+	sudo docker-compose build | tee ${logfile}
+else
+	echo "log directory does not exist, or is not writable..."
+	exit 3
+fi
 
 if [ $? != 0 ]; then
 	echo "sudo docker-compose build failed($?): see ${logfile} for details"
-	exit 2
+	exit 4
 fi
+
+# XXX there must be a better way to grant write perms to containers
+chmod ugo+rwx log run etc/varnishstats
 
 exit 0
